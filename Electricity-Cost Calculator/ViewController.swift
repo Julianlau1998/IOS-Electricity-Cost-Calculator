3a2b04add26519114e02b41f1dcec8469c7e161d
Julian Lau <julian.lau@aol.com>
2022-08-21 17:08:23 +0200

    Initial Commit

---
+++ Electricity-Cost Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Electricity-Cost Calculator
+//
+//  Created by Julian Lau on 21.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
